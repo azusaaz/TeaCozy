cf69634aa41ff41e77693b4fcc4308492763569d
azusaaz <ashimazaki@myseneca.ca>
2018-06-05 17:21:42 -0400

    decoration added

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+//scroll
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({
+            scrollTop: position
+        }, 300, 'swing');
+        return false;
+    });
+});
